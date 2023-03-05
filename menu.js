8ed88cbfd4e492cfbb772942272ce7d12b12caa2
eriancoet <eriancoet@gmail.com>
2023-03-05 23:13:47 +0200

    commit files

---
+++ menu.js
@@ -0,0 +1,15 @@
+var imageThumbs = document.getElementById("image-thumbs");
+var currentImage = document.getElementById("current-image");
+
+for (var i = 1; i <= 9; i++) {
+  var thumb = document.createElement("img");
+  thumb.src = "menu/menu" + i + ".png";
+  thumb.alt = "menu page " + i;
+  thumb.classList.add("thumb");
+  imageThumbs.appendChild(thumb);
+  thumb.addEventListener(
+    "click", function() {
+      currentImage.src = this.src;
+    }
+  );
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$("nav").fadeIn("5000")
+ 
+});
+
+$(document).ready(function(){
+  $("img").fadeIn("5000")
+   
+  });
+
+
