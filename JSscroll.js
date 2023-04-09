c4a6ab63025111a8d9cc24bb548d4035f0ce8927
Rian Coetzee <eriancoet@gmail.com>
2023-04-09 21:25:22 +0200

    Ek is Afrikaans

--- JSscroll.js
+++ JSscroll.js
@@ -1,5 +1,5 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+    var reveals = document.querySelectorAll(".reveal", ".reveal1");
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
@@ -16,4 +16,25 @@ function reveal() {
 
 // To check the scroll position on page load
 reveal();
+
+
+function active() {
+  var reveals = document.querySelectorAll(".active");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 130;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", active);
+
+// To check the scroll position on page load
+active();
+
 
